d0e75addc870addbb865cdf787d7c2a2bc5a9cc6
adish17 <adishirfan17@gmail.com>
2025-07-05 17:11:31 +0530

    id is coming as null in user data

--- app/src/main/java/com/laundrypro/app/models/User.kt
+++ app/src/main/java/com/laundrypro/app/models/User.kt
@@ -1,9 +1,13 @@
 package com.laundrypro.app.models
 
+import com.google.gson.annotations.SerializedName
+
+// The User model is updated to correctly map the "_id" field from the server.
 data class User(
-    val id: String,
-    val name: String,
-    val email: String,
-    val phone: String,
-    val addresses: List<Address>? = emptyList()
+    @SerializedName("_id")
+    val id: String?,
+    val name: String?,
+    val email: String?,
+    val phone: String?,
+    val address: Address?
 )

e8c4d269f8b6e70607b85221c51fb3bffb5121a5
adish17 <adishirfan17@gmail.com>
2025-08-24 23:40:48 +0530

    refactor profile page according to new UX

--- app/src/main/java/com/dhobikart/app/fragments/ProfileFragment.kt
+++ app/src/main/java/com/dhobikart/app/fragments/ProfileFragment.kt
@@ -2,7 +2,6 @@ package com.dhobikart.app.fragments
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.dhobikart.app.R
@@ -10,6 +9,7 @@ import com.dhobikart.app.databinding.FragmentProfileBinding
 import com.dhobikart.app.viewmodels.LaundryViewModel
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
     private val viewModel: LaundryViewModel by activityViewModels()
@@ -18,51 +18,57 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentProfileBinding.bind(view)
 
-        setupClickListeners()
-        observeViewModel()
-    }
+        binding.toolbar.setNavigationOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
 
-    private fun setupClickListeners() {
-        binding.btnEditProfile.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, EditProfileFragment())
-                .addToBackStack(null)
-                .commit()
-        }
-        binding.btnManageAddresses.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, ManageAddressesFragment())
-                .addToBackStack(null)
-                .commit()
-        }
-        binding.btnNotificationSettings.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, NotificationSettingsFragment())
-                .addToBackStack(null)
-                .commit()
-        }
-        binding.btnHelp.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, HelpSupportFragment())
-                .addToBackStack(null)
-                .commit()
-        }
-        binding.btnLogout.setOnClickListener {
-            viewModel.logout()
-        }
+        observeViewModel()
+        setupClickListeners()
     }
 
     private fun observeViewModel() {
         viewModel.currentUser.observe(viewLifecycleOwner) { user ->
             if (user != null) {
-                binding.tvUserName.text = user.name
-                binding.tvUserEmail.text = user.email
-            } else {
-                // This state should rarely be seen now because of the navigation guard,
-                // but it's good practice to handle it.
-                binding.tvUserName.text = "Guest User"
-                binding.tvUserEmail.text = ""
+                binding.profileName.text = user.name
+                // In a real app, you would load the profile image with Glide or Picasso
+                // binding.profileImage.load(user.profileImageUrl)
             }
+        }
+    }
+
+    private fun setupClickListeners() {
+        binding.btnPersonalInfo.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, EditProfileFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+        binding.btnContactInfo.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, EditProfileFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+        binding.btnAddress.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ManageAddressesFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+        binding.btnNotifications.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, NotificationSettingsFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+        binding.btnPaymentMethods.setOnClickListener {
+            // TODO: Navigate to a new PaymentMethodsFragment
+        }
+        binding.btnOrderHistory.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, OrdersFragment())
+                .addToBackStack(null)
+                .commit()
         }
     }
 
